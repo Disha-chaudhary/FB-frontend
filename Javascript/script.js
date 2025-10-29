618f344425564d6d028b6fa07031e5353664af31
Disha chaudhary <disha.chaudhary_cs24@gla.ac.in>
2025-10-29 11:48:04 -1200

    arrays

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,32 @@
 // console.log("hello")
 // alert("welcome to this site")
-const name=prompt("enter the user name:")
-console.log(name)
-alert("hello the name is"+name)
+// const name=prompt("enter the user name:")
+// console.log(name)
+// alert("hello the name is"+name)
+// document.write("<h1>hello,"+ name + "!</h1>")
+// const age=prompt("enter the age")
+// if(age>18){
+//    document.write("<h1>you are eligible</h1>")
+// }
+// else{
+//     alert("you are not eligible")
+// }
+    
+//ARRAY:-
+
+//const arr=[1,2,3,4,"disha",undefined,NaN,false]
+//const arr=new Array(8)
+// const arr=new Array("abc","def","gei")
+const start=[1,2,3,4]
+// const end=[5,6,7,8]
+//const newArr=[];
+// for(let num of start){
+//     newArr.push(num)
+// }
+// for(let num of end){
+//     newArr.push(num)
+// }
+// const newArr=start.concat(end)
+// console.log(newArr)
+//METHODS: push,arrayname.indexof(element),arrayname.concat(secondarrayname)
+console.log(start.pop())
