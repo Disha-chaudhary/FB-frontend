fbde8c888aacf190a9a53305414b27050d6be8ba
Disha chaudhary <disha.chaudhary_cs24@gla.ac.in>
2025-11-20 10:23:42 -1200

    arrowFn

---
+++ Javascript/arrowfunctions.js
@@ -0,0 +1,25 @@
+  
+//  let  name="disha";
+//    let  age=18;
+//   function welcomeMessage(){
+//     console.log(`${this.name},welcome to website you age is${age}`);
+    
+    
+    
+//   }
+//   welcomeMessage();
+
+//ARROW FUNCTION:-
+
+// let name="disha"
+// const chai=()=>{
+//     console.log(this.name)
+//     //arrow functions does not allow this key word
+
+// }
+// chai()
+
+// const addtwo=(num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(addtwo(9,1))
